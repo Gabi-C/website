1d5c4f1f10dd83b09105f69b0de5cd3df96b4101
GabiC <gabi_cicconeto@hotmail.com>
2018-03-04 15:29:34 +0800

    Changes4

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,37 @@
+/*
+** Document is ready
+*/
 $(document).ready(function(){
 
-  //show a message
-  console.log('I am ready to show!');
+  /*
+  **  Changes the body class to show / hide the mobile menu
+  */
+  function toggleMobileMenu() {
+
+      console.log('Ready');
+
+    // Check if the body has the "mobile-nav-visible" class
+    if ($('body').hasClass('mobile-nav-visible')) {
+      $('body').removeClass('mobile-nav-visible');
+    } else {
+      $('body').addClass('mobile-nav-visible');
+    }
+
+  }
+
+  // Bind
+  $(document).on('click', '#hamburger', toggleMobileMenu);
+
+});
+
+/*
+** Document is ready and all assets (images, video's etc. etc.) are loaded
+*/
+$(window).on('load', function() {
+
+  // Attach Flexslider to classname .flexslider
+  $('.flexslider').flexslider({
+    animation: "fade"
+  });
 
 });
